fb56b3681ccba641c3ee1be8eef5637ee7c80869
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2024-06-21 22:01:55 +0300

    Remove ReactStrict mode

--- src/pages/Tests.jsx
+++ src/pages/Tests.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import {LOCAL_URL} from "../../config.js";
 import Test from "../components/Test.jsx";
 import { Flex } from "antd";
-import { Link } from "react-router-dom";
 
 
 export default function Tests() {
@@ -18,7 +17,7 @@ export default function Tests() {
 
     return (
         <Flex wrap={true} style={{minHeight: "100vh", padding:"1rem"}} justify="space-evenly" align="center" gap="1rem">
-            {tests.map(test => <Link key={test._id} to={`/tests/${test._id}`}><Test test={test}/></Link>)}
+            {tests.map(test => <Test key={test._id} test={test}/>)}
         </Flex>
     )
 }
