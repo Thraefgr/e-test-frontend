4bb1632271f77c15199ad02670797a54240c4ee6
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2024-06-24 01:10:14 +0300

    Add a update floating button to UpdateCreation page

--- src/pages/UpdateCreation.jsx
+++ src/pages/UpdateCreation.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import { LOCAL_URL } from "../../config";
 import axios from "axios";
 import Creation from "../components/Creation.jsx";
+import { FloatButton, message } from "antd";
+import { SaveFilled, SaveOutlined } from "@ant-design/icons";
 
 export default function UpdateCreation() {
     const token = JSON.parse(localStorage.getItem("user")).token;
@@ -12,7 +14,18 @@ export default function UpdateCreation() {
         axios.get(`${LOCAL_URL}/mycreation/${id}`, {headers:{"Authorization":`Bearer ${token}`}})
         .then(res => setTest(res.data))
     }, [])
+
+    const updateTest = () => {
+        test.queCount = test.questions.length;
+        axios.put(`${LOCAL_URL}/mycreation/${id}`, test, {headers:{"Authorization": `Bearer ${token}`}})
+        .then(() => message.success("Successully updated your test!"))
+        .catch(() => message.error("Could not update the test. Sorry..."))
+    }
     return (
-        test ? <Creation data={[test, setTest]} /> : <h1></h1>
+        test ? 
+        <>
+            <Creation data={[test, setTest]} />
+            <FloatButton style={{right:"96px"}} icon={<SaveOutlined />} onClick={updateTest}>Click</FloatButton>
+        </> : <h1></h1>
     )
 }
