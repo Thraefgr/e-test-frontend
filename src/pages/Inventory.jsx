cc2d0ab195433bfe2fd9356b4b2503bb4cae0cab
Ferhat Yenilmez <118694958+Thraefgr@users.noreply.github.com>
2024-06-25 23:35:23 +0300

    Change unique key from id to index

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -18,7 +18,7 @@ export default function Inventory() {
 
     return (
         <Flex wrap gap="1rem" style={{padding:"1rem"}}>
-            {owned?.map(test => <InventoryCard key={test?.testId?._id} test={test} own={[setOwned]}/>)}
+            {owned?.map((test, i) => <InventoryCard key={i} test={test} own={[setOwned]}/>)}
         </Flex>
     )
 }
